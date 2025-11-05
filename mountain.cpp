6d39feecce1660f2637cc31daeea628f1d8e2f7e
P217F <vuongiavinhdieu2k9@gmail.com>
2025-11-05 17:56:41 +0700

    #16: Add mountain.cpp

--- mountain.cpp
+++ mountain.cpp
@@ -2,10 +2,32 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin>>n;
-    vector<long int> arr(n+1);
-    for(int i=1;i<=n;++i) {
-        cin>>arr[i];
+  int n;
+  cin >> n;
+  vector<long long> arr(n);
+  for (int i = 0; i < n; ++i)
+    cin >> arr[i];
+
+  int length = 0;
+  int i = 1;
+  while (i < n) {
+    while (i < n && arr[i] == arr[i - 1])
+      i++;
+    int up = 0, down = 0;
+
+    while (i < n && arr[i] > arr[i - 1]) {
+      up++;
+      i++;
     }
+    while (i < n && arr[i] < arr[i - 1]) {
+      down++;
+      i++;
+    }
+
+    if (up > 0 && down > 0)
+      length = max(length, up + down + 1);
+  }
+
+  cout << length << endl;
+  return 0;
 }
